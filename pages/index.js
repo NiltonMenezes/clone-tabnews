990d46be2c174174fb9fd75621f841a350e5de96
Nilton Menezes da Silva Junior <niltonmenezes@live.com>
2025-10-13 08:39:29 -0300

    Add .next to .gitignore, update package.json scripts, and create Home component in index.js

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Raí, continue seguindo em frente e lutando que você vai chegar lá e as coisas vão sim melhorar! O segredo é persistir!</h1>
+}
+
+export default Home;
