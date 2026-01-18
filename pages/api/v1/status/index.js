76c2fb12bd47ccbc9675201e591832f5935b6a2f
NiltonMenezes <niltonmenezes@live.com>
2026-01-18 17:17:46 -0300

    add dotenv and database configuration

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function statusHandler(req, res) {
+import database from '../../../../infra/database.js'
+
+async function statusHandler(req, res) {
+  console.log('Database connection test result:', await database.query('SELECT 1'));
   res.status(200).json({ status: 'ok' });
 }
 
