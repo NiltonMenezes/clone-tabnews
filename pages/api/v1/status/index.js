25abb86cb37c7876f5d33ec520465025afe9bf3b
NiltonMenezes <niltonmenezes@live.com>
2026-01-19 09:17:58 -0300

    add jsconfig.json to use absolute path

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from '../../../../infra/database.js'
+import database from 'infra/database.js'
 
 async function statusHandler(req, res) {
-  console.log('Database connection test result:', await database.query('SELECT 1'));
-  res.status(200).json({ status: 'ok' });
+  console.log('Database connection test result:', await database.query('SELECT 1'))
+  res.status(200).json({ status: 'ok' })
 }
 
-export default statusHandler;
+export default statusHandler
