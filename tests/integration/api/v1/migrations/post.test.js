1dc79fb05beabb5cf398d4dcbf182b310dc738f8
NiltonMenezes <niltonmenezes@live.com>
2026-01-30 23:25:48 -0300

    add jest config

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database.js"
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase(){
+    await database.query("drop schema public cascade; create schema public;")
+}
+
 test("POST /api/v1/migrations returns 200 and the updated_at on format ISO8601", async () => {
     const response = await fetch("http://localhost:3000/api/v1/migrations", {
         method: "POST"
