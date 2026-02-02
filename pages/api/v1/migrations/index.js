57c8d04d327e8dbc36e5612f7f7405776e767cb2
NiltonMenezes <niltonmenezes@live.com>
2026-02-02 07:13:11 -0300

    refactor and more tests

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,28 +2,28 @@ import migrationRunner from 'node-pg-migrate'
 import { join } from 'node:path'
 
 export default async function migrationsHandler(req, res) {
-  if(req.method === 'GET') {
-    const migrations = await migrationRunner({
+  const defaultMigrationOption = {
       databaseUrl: process.env.DATABASE_URL,
       dryRun: true,
       dir: join('infra', 'migrations'),
       direction: 'up',
       verbose: true,
       migrationsTable: 'pgmigrations',
-    })
-    return res.status(200).json(migrations)
+    }
+  
+  if(req.method === 'GET') {
+    const pendingMigrations = await migrationRunner(defaultMigrationOption)
+    return res.status(200).json(pendingMigrations)
   }
 
   if(req.method === 'POST') {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pgmigrations',
-    })
-    return res.status(200).json(migrations)
+    const migratedMigrations = await migrationRunner({ ...defaultMigrationOption, dryRun: false })
+    
+    if(migratedMigrations.length > 0) {
+      return res.status(201).json(migratedMigrations)
+    }
+
+    return res.status(200).json(migratedMigrations)
   }
 
   return res.status(405).end()
