ec8aa4872447ecc96488e28229de4fa3f8fc6db3
NiltonMenezes <niltonmenezes@live.com>
2026-09-05 18:01:37 -0300

    refactor: error padronization

--- infra/database.js
+++ infra/database.js
@@ -12,7 +12,7 @@ async function query(queryObject) {
     const serviceErrorObject = new ServiceError({
       message: "Erro na conexão com o banco ou na query.",
       cause: error,
-      statusCode: error.statusCode,
+      statusCode: 503,
     });
     throw serviceErrorObject;
   } finally {
