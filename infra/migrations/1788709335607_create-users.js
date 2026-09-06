e30c8b986e6c3515d9c02a3f31ac22b3af818e20
NiltonMenezes <niltonmenezes@live.com>
2026-09-06 18:14:24 -0300

    test: add tests for endpoint to create new users

--- infra/migrations/1788709335607_create-users.js
+++ infra/migrations/1788709335607_create-users.js
@@ -1,44 +1,42 @@
 exports.up = (pgm) => {
-    pgm.createTable("users", {
-        id: {
-            type: "uuid",
-            primaryKey: true,
-            default: pgm.func("gen_random_uuid()")
-        },
-        
-        // For reference, Github limits usernames to 39 characters.
-        username: {
-            type: "varchar(30)",
-            notNull: true,
-            unique: true
-        },
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
 
-        // Why 254 in length? https://stackoverflow.com/a/1199238
-        email: {
-            type: "varchar(254)",
-            notNull: true,
-            unique: true
-        },
+    // For reference, Github limits usernames to 39 characters.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
 
-        // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
-        password: {
-            type: "varchar(60)",
-            notNull: true
-        },
+    // Why 254 in length? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
 
-        // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
-        created_at: {
-            type: "timestamptz",
-            default: pgm.func("timezone('utc', now())"),
-            notNull: true
-        },
+    // Why 72 in length? https://security.stackexchange.com/a/39851
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
 
-        updated_at: {
-            type: "timestamptz",
-            default: pgm.func("timezone('utc', now())"),
-            notNull: true
-        }
-    })
+    // Why timestamp with time zone? https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+  });
 };
 
 exports.down = false;
---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,119 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "john_doe",
+          email: "john.doe@example.com",
+          password: "password123",
+        }),
+      });
+
+      expect(response.status).toBe(201);
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "john_doe",
+        email: "john.doe@example.com",
+        password: "password123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With duplicated email", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailduplicado1",
+          email: "emailduplicado@example.com",
+          password: "password123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailduplicado2",
+          email: "EmailDuplicado@example.com",
+          password: "password123",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está em uso.",
+        action: "Por favor, utilize outro email para se cadastrar.",
+        status_code: 400,
+      });
+    });
+
+    test("With duplicated userName", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "usernameduplicado",
+          email: "usernameduplicado1@example.com",
+          password: "password123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "UserNameduplicado",
+          email: "usernameduplicado2@example.com",
+          password: "password123",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O nome de usuário informado já está em uso.",
+        action: "Por favor, utilize outro nome de usuário para se cadastrar.",
+        status_code: 400,
+      });
+    });
+  });
+});
